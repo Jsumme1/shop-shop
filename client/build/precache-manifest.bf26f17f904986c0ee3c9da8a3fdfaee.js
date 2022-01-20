601ee2db00d482c6b29691b68c8537952cb82a48
Jsumme1 <88805050+Jsumme1@users.noreply.github.com>
2022-01-20 16:08:11 -0600

    build folder

---
+++ client/build/precache-manifest.bf26f17f904986c0ee3c9da8a3fdfaee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2b83dd4f58e17f6e18bb38f941ea85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f025f96c849bff677472",
+    "url": "/static/css/main.ef812045.chunk.css"
+  },
+  {
+    "revision": "c6847b4b757e4ec9a4c3",
+    "url": "/static/js/2.a1766ba2.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.a1766ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f025f96c849bff677472",
+    "url": "/static/js/main.1294cb94.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
